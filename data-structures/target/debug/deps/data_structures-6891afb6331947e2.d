c93b007b7042f1372b82e1ccb915ff41038ebfe5
Erick V. <javillatoro1@gmail.com>
2024-10-15 21:48:46 -0600

    Removing target folder

--- data-structures/target/debug/deps/data_structures-6891afb6331947e2.d
+++
@@ -1,5 +0,0 @@
-/home/erick/Documentos/Rust/data-structures/target/debug/deps/libdata_structures-6891afb6331947e2.rmeta: src/main.rs
-
-/home/erick/Documentos/Rust/data-structures/target/debug/deps/data_structures-6891afb6331947e2.d: src/main.rs
-
-src/main.rs:
