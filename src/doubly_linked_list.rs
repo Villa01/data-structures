dac8249824bebd5366492e6a47a61020967314b7
Erick V. <javillatoro1@gmail.com>
2024-11-12 20:08:17 -0600

    Change docs to update crate name

--- src/doubly_linked_list.rs
+++ src/doubly_linked_list.rs
@@ -8,7 +8,7 @@ impl<T> DoublyLinkedList<T> {
     ///
     /// # Examples
     /// ```
-    /// use data_structures::doubly_linked_list::DoublyLinkedList;
+    /// use villa01_data_structures::doubly_linked_list::DoublyLinkedList;
     /// struct T;
     /// let list:DoublyLinkedList<T> = DoublyLinkedList::new();
     /// assert!(list.is_empty());
@@ -29,7 +29,7 @@ impl<T> DoublyLinkedList<T> {
     /// # Examples
     ///
     /// ```
-    /// use data_structures::doubly_linked_list::DoublyLinkedList;
+    /// use villa01_data_structures::doubly_linked_list::DoublyLinkedList;
     /// let list = DoublyLinkedList::with_value(42);
     /// assert_eq!(list.length, 1);
     /// ```
@@ -48,16 +48,29 @@ impl<T> DoublyLinkedList<T> {
     ///
     /// # Examples
     /// ```
-    /// use data_structures::doubly_linked_list::DoublyLinkedList;
+    /// use villa01_data_structures::doubly_linked_list::DoublyLinkedList;
     ///
     /// let empty_list: DoublyLinkedList<i32> = DoublyLinkedList::new();
     /// assert!(empty_list.is_empty());
     ///
-    /// let list: DoublyLinkedList<i32> = DoublyLinkedList::with_value(30);
+    /// let list: DoublyLinkedList<i32> = DoublyLinkedList::with_value(69);
     /// assert!(!list.is_empty());
     /// ```
     pub fn is_empty(&self) -> bool {
-        self.length == 0
+        self.len() == 0
+    }
+
+    /// Gets the amount of items in the `DoublyLinkedList`.
+    ///
+    /// # Examples
+    /// ```
+    /// use villa01_data_structures::doubly_linked_list::DoublyLinkedList;
+    ///
+    /// let list: DoublyLinkedList<i32> = DoublyLinkedList::with_value(69);
+    /// assert_eq!(list.len(),1);
+    /// ```
+    pub fn len(&self) -> usize {
+        self.length
     }
 }
 
@@ -84,10 +97,10 @@ mod tests {
             assert!(list.is_empty())
         }
 
+        #[test]
         fn test_with_value() {
             let list: DoublyLinkedList<bool> = DoublyLinkedList::with_value(true);
             assert!(!list.is_empty())
         }
-
     }
 }
