b9f9cf9fc6f62b87dc66d8feb0a8604214226dfc
Erick V. <javillatoro1@gmail.com>
2024-11-12 18:35:45 -0600

    Create base structure for doubly linked list

---
+++ src/doubly_linked_list.rs
@@ -0,0 +1,93 @@
+pub struct DoublyLinkedList<T> {
+    pub head: Option<Box<ListNode<T>>>,
+    pub length: usize,
+}
+
+impl<T> DoublyLinkedList<T> {
+    /// Creates a new empty `DoublyLinkedList`.
+    ///
+    /// # Examples
+    /// ```
+    /// use data_structures::doubly_linked_list::DoublyLinkedList;
+    /// struct T;
+    /// let list:DoublyLinkedList<T> = DoublyLinkedList::new();
+    /// assert!(list.is_empty());
+    /// ````
+    pub fn new() -> DoublyLinkedList<T> {
+        DoublyLinkedList {
+            head: None,
+            length: 0,
+        }
+    }
+
+    /// Creates a new `DoublyLinkedList` initialized with a single element.
+    ///
+    /// # Parameters
+    ///
+    /// - `data`: The initial value for the first node in the list.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use data_structures::doubly_linked_list::DoublyLinkedList;
+    /// let list = DoublyLinkedList::with_value(42);
+    /// assert_eq!(list.length, 1);
+    /// ```
+    pub fn with_value(data: T) -> DoublyLinkedList<T> {
+        let new_node = Box::new(ListNode::new(data));
+        DoublyLinkedList {
+            head: Some(new_node),
+            length: 1,
+        }
+    }
+
+    /// Checks if the `DoublyLinkedList` is empty.
+    ///
+    /// # Returns
+    /// `true` if the list has no elements, and `false` otherwise
+    ///
+    /// # Examples
+    /// ```
+    /// use data_structures::doubly_linked_list::DoublyLinkedList;
+    ///
+    /// let empty_list: DoublyLinkedList<i32> = DoublyLinkedList::new();
+    /// assert!(empty_list.is_empty());
+    ///
+    /// let list: DoublyLinkedList<i32> = DoublyLinkedList::with_value(30);
+    /// assert!(!list.is_empty());
+    /// ```
+    pub fn is_empty(&self) -> bool {
+        self.length == 0
+    }
+}
+
+pub struct ListNode<T> {
+    next: Option<Box<ListNode<T>>>,
+    pub data: T,
+}
+
+impl<T> ListNode<T> {
+    pub fn new(data: T) -> ListNode<T> {
+        ListNode { data, next: None }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    mod doubly_linked_list {
+        use crate::doubly_linked_list::DoublyLinkedList;
+
+        #[test]
+        fn test_new() {
+            let list: DoublyLinkedList<bool> = DoublyLinkedList::new();
+            assert!(list.is_empty())
+        }
+
+        fn test_with_value() {
+            let list: DoublyLinkedList<bool> = DoublyLinkedList::with_value(true);
+            assert!(!list.is_empty())
+        }
+
+    }
+}
