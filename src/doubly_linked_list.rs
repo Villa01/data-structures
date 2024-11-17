64248734fd8b81aa38636f5e116e804afbe75af4
Erick V. <javillatoro1@gmail.com>
2024-11-16 21:30:50 -0600

    Create get_last method

--- src/doubly_linked_list.rs
+++ src/doubly_linked_list.rs
@@ -336,12 +336,12 @@ impl<T> DoublyLinkedList<T> {
     /// use villa01_data_structures::doubly_linked_list::DoublyLinkedList;
     ///
     /// let mut list = DoublyLinkedList::new();
-    /// assert_eq!(list.get_first(), None);
+    /// assert_eq!(list.get_first_mut(), None);
     ///
     /// list.insert_at_beginning(42);
     /// list.insert_at_beginning(84);
     ///
-    /// assert_eq!(list.get_first(), Some(&84));
+    /// assert_eq!(list.get_first_mut(), Some(&mut 84));
     /// ```
     /// # Complexity
     /// - `O(1)`
@@ -349,6 +349,66 @@ impl<T> DoublyLinkedList<T> {
     pub fn get_first_mut(&self) -> Option<&mut T> {
         match self.head {
             Some(head) => unsafe { return Some(&mut (*head).data) },
+            None => None,
+        }
+    }
+
+    /// Retrieves a reference to the last element in the `DoublyLinkedList`, if it exists.
+    ///
+    /// # Returns
+    ///
+    /// - `Some(&T)`: A reference to the last element in the list.
+    /// - `None`: If the list is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use villa01_data_structures::doubly_linked_list::DoublyLinkedList;
+    ///
+    /// let mut list = DoublyLinkedList::new();
+    /// assert_eq!(list.get_last(), None);
+    ///
+    /// list.insert_at_beginning(42);
+    /// list.insert_at_beginning(84);
+    ///
+    /// assert_eq!(list.get_last(), Some(&42));
+    /// ```
+    /// # Complexity
+    /// - `O(1)`
+    ///
+    pub fn get_last(&self) -> Option<&T> {
+        match self.tail {
+            Some(tail) => unsafe { return Some(&(*tail).data) },
+            None => None,
+        }
+    }
+
+    /// Retrieves a reference to the last element in the `DoublyLinkedList`, if it exists.
+    ///
+    /// # Returns
+    ///
+    /// - `Some(&mut T)`: A mutable reference to the last element in the list.
+    /// - `None`: If the list is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use villa01_data_structures::doubly_linked_list::DoublyLinkedList;
+    ///
+    /// let mut list = DoublyLinkedList::new();
+    /// assert_eq!(list.get_last_mut(), None);
+    ///
+    /// list.insert_at_beginning(42);
+    /// list.insert_at_beginning(84);
+    ///
+    /// assert_eq!(list.get_last_mut(), Some(&mut 42));
+    /// ```
+    /// # Complexity
+    /// - `O(1)`
+    ///
+    pub fn get_last_mut(&self) -> Option<&mut T> {
+        match self.tail {
+            Some(tail) => unsafe { return Some(&mut (*tail).data) },
             None => None,
         }
     }
@@ -668,6 +728,64 @@ mod tests {
                 (*ivalue) += 1;
 
                 let new_value = list.get_first().unwrap();
+                assert_eq!(value + 1, *(new_value));
+            }
+        }
+
+        mod get_last {
+            use super::*;
+
+            #[test]
+            fn test_get_last_empty_list() {
+                let list: DoublyLinkedList<usize> = DoublyLinkedList::new();
+                let i = list.get_last();
+
+                assert!(i.is_none());
+            }
+
+            #[test]
+            fn test_get_last() {
+                let value = 10;
+                let list: DoublyLinkedList<usize> = DoublyLinkedList::with_value(value);
+                let i = list.get_last();
+
+                assert!(i.is_some());
+                assert_eq!(value, *(i.unwrap()));
+            }
+            #[test]
+            fn test_get_last_verify_inmutable() {
+                let value = 10;
+                let list: DoublyLinkedList<usize> = DoublyLinkedList::with_value(value);
+                let i = list.get_last();
+
+                assert!(i.is_some());
+                assert_eq!(value, *(i.unwrap()));
+            }
+        }
+
+        mod get_last_mut {
+            use super::*;
+
+            #[test]
+            fn test_get_last_empty_list() {
+                let list: DoublyLinkedList<usize> = DoublyLinkedList::new();
+                let i = list.get_last_mut();
+
+                assert!(i.is_none());
+            }
+
+            #[test]
+            fn test_get_last() {
+                let value = 10;
+                let list: DoublyLinkedList<usize> = DoublyLinkedList::with_value(value);
+                let i = list.get_last_mut();
+                assert!(i.is_some());
+
+                let ivalue = i.unwrap();
+                assert_eq!(value, *(ivalue));
+                (*ivalue) += 1;
+
+                let new_value = list.get_last().unwrap();
                 assert_eq!(value + 1, *(new_value));
             }
         }
